ac61afb783020c82b950d5d79ef1d3a5aa2e9a3b
liuxin03014 <liuxin03014@gmail.com>
2024-11-16 00:13:28 +0800

    Site updated: 2024-11-16 00:13:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/8ab2dce2.html","posts/8f5ec72c.html","posts/b18bdd93.html","posts/b18bdd93.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/8f5ec72c.html","posts/13bb8d58.html","posts/b18bdd93.html","posts/8ab2dce2.html","posts/b18bdd93.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
