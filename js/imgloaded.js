bf66f16ef049d5b1ecdb9ad351b0c185489a3ae5
liuxin03014 <liuxin03014@gmail.com>
2024-11-29 14:40:37 +0800

    Site updated: 2024-11-29 14:40:37

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -78,10 +78,10 @@
   };
   
   const config = {
-    smallSrc: 'https://t.mwm.moe/fj', // 小图链接 尽可能配置小于100k的图片
-    largeSrc: 'https://t.mwm.moe/fj', // 大图链接 最终显示的图片
-    mobileSmallSrc: 'https://t.mwm.moe/fj', // 手机端小图链接 尽可能配置小于100k的图片
-    mobileLargeSrc: 'https://t.mwm.moe/fj', // 手机端大图链接 最终显示的图片
+    smallSrc: 'https://api.qjqq.cn/api/Img?sort=acg', // 小图链接 尽可能配置小于100k的图片
+    largeSrc: 'https://api.qjqq.cn/api/Img?sort=acg', // 大图链接 最终显示的图片
+    mobileSmallSrc: 'https://api.qjqq.cn/api/Img?sort=acg', // 手机端小图链接 尽可能配置小于100k的图片
+    mobileLargeSrc: 'https://api.qjqq.cn/api/Img?sort=acg', // 手机端大图链接 最终显示的图片
     enableRoutes: ['/'],
     };
   
